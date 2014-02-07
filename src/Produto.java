36b3664b0ba158d46e679086b5f998f2353e1b6f
unknown <fabio.junior@dce.ufpb.br>
2014-02-07 13:36:43 -0200

    supermercado

---
+++ src/Produto.java
@@ -0,0 +1,10 @@
+
+public class Produto {
+	private int codigo;
+	private String nome;
+	
+	public Produto(int codigo, String nome){
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+}
--- src/Supermercado.java
+++ src/Supermercado.java
@@ -1,4 +1,11 @@
 
 public class Supermercado {
+	private String nome;
+	private String cidade;
+	
+	public Supermercado(String nome, String cidade){
+		this.nome = nome;
+		this.cidade = cidade;
+	}
 
 }

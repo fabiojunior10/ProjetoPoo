f5200d076c10aaad8358964859f935a0b7657a28
unknown <fabio.junior@dce.ufpb.br>
2014-02-07 15:53:29 -0200

    funcionarios

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,7 +1,11 @@
 public class Funcionario {
 	private String nome;
+	private String fone;
+	private String cidade;
 	
-	public Funcionario(String nome){
+	public Funcionario(String nome, String fone, String cidade){
 		this.nome = nome;
+		this.fone = fone;
+		this.cidade = cidade;
 	}
 }

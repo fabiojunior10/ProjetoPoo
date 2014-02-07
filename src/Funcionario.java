8252c598c7e38e08d977946954ea50cc2d0182a4
unknown <fabio.junior@dce.ufpb.br>
2014-02-07 16:27:25 -0200

    commit 3

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -2,10 +2,12 @@ public class Funcionario {
 	private String nome;
 	private String fone;
 	private String cidade;
+	private String rg;
 	
-	public Funcionario(String nome, String fone, String cidade){
+	public Funcionario(String nome, String fone, String cidade, String rg){
 		this.nome = nome;
 		this.fone = fone;
 		this.cidade = cidade;
+		this.rg = rg;
 	}
 }
